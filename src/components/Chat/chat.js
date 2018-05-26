5d0abc4e5691d99f04c607003d273bbf8121173b
shgtkshruch <shgtk.shruch@gmail.com>
2018-05-26 18:09:02 +0900

    Add vuex

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,15 +1,21 @@
+import { mapGetters, mapActions } from 'vuex';
+import { SET_MESSAGE } from '../../store/mutation-types';
+
 export default {
   name: 'Chat',
+  computed: {
+    ...mapGetters(['messages']),
+  },
   data() {
     return {
       channels: ["general", "random"],
       message: '',
-      messages: []
     }
   },
   methods: {
+    ...mapActions([ SET_MESSAGE ]),
     send_message(){
-      this.messages.push(this.message);
+      this.SET_MESSAGE(this.message)
       this.message = "";
     }
   }
---
+++ src/store/actions.js
@@ -0,0 +1,7 @@
+import { SET_MESSAGE } from './mutation-types';
+
+export default {
+  [SET_MESSAGE] ({commit}, message) {
+    commit(SET_MESSAGE, message)
+  },
+}
